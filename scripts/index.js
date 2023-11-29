accae275974cfc5d4965202e579df08ff4d9462a
Dmitriy Pervin <theforner@gmail.com>
2023-11-29 19:02:29 +0500

    Add create, delete and render functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const placeCardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createPlaceCard(titleValue, imageSourceValue) {
+  placeCardElement = placeCardTemplate.querySelector('.places__item').cloneNode(true);
+  placeCardElement.querySelector('.card__title').textContent = titleValue;
+  placeCardElement.querySelector('.card__image').src = imageSourceValue;
+  placeCardElement.querySelector('.card__delete-button').addEventListener('click', removePlaceCard);
+  return placeCardElement;
+}
 
 // @todo: Функция удаления карточки
+function removePlaceCard(event) {
+  event.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderPlaceList() {
+  let places = [];
+  initialCards.forEach((item) => {
+    places.push(createPlaceCard(item.name, item.link));
+  });
+  placeList.append(...places);
+}
+
+renderPlaceList();
